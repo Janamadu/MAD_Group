4d46012e84fb6db8570e3808add63def36d8a33f
janamadu <it18159240@my.sliit.lk>
2020-04-30 13:30:51 +0530

    2nd commit

---
+++ app/src/main/java/com/example/mad_new/BrighterBee.java
@@ -0,0 +1,111 @@
+package com.example.mad_new;
+import androidx.annotation.NonNull;
+
+import java.util.Date;
+
+public class BrighterBee {
+
+    private Integer StudentName;
+    private Integer Clas;
+    private Integer Index;
+    private Integer Sub1Mark;
+    private Integer Sub2Mark;
+    private Integer Sub3Mark;
+    private Integer Sub4Mark;
+    private Integer Sub5Mark;
+    private Integer TotMarks;
+    private Integer Avg;
+    private Integer Posi;
+
+    public BrighterBee() {
+    }
+
+    public Integer getStudentName() {
+        return StudentName;
+    }
+
+    public void setStudentName(Integer studentName) {
+        StudentName = studentName;
+    }
+
+    public Integer getClas() {
+        return Clas;
+    }
+
+    public void setClas(Integer clas) {
+        Clas = clas;
+    }
+
+    public Integer getIndex() {
+        return Index;
+    }
+
+    public void setIndex(Integer index) {
+        Index = index;
+    }
+
+    public Integer getSub1Mark() {
+        return Sub1Mark;
+    }
+
+    public void setSub1Mark(Integer sub1Mark) {
+        Sub1Mark = sub1Mark;
+    }
+
+    public Integer getSub2Mark() {
+        return Sub2Mark;
+    }
+
+    public void setSub2Mark(Integer sub2Mark) {
+        Sub2Mark = sub2Mark;
+    }
+
+    public Integer getSub3Mark() {
+        return Sub3Mark;
+    }
+
+    public void setSub3Mark(Integer sub3Mark) {
+        Sub3Mark = sub3Mark;
+    }
+
+    public Integer getSub4Mark() {
+        return Sub4Mark;
+    }
+
+    public void setSub4Mark(Integer sub4Mark) {
+        Sub4Mark = sub4Mark;
+    }
+
+    public Integer getSub5Mark() {
+        return Sub5Mark;
+    }
+
+    public void setSub5Mark(Integer sub5Mark) {
+        Sub5Mark = sub5Mark;
+    }
+
+    public Integer getTotMarks() {
+        return TotMarks;
+    }
+
+    public void setTotMarks(Integer totMarks) {
+        TotMarks = totMarks;
+    }
+
+    public Integer getAvg() {
+        return Avg;
+    }
+
+    public void setAvg(Integer avg) {
+        Avg = avg;
+    }
+
+    public Integer getPosi() {
+        return Posi;
+    }
+
+    public void setPosi(Integer posi) {
+        Posi = posi;
+    }
+}
+
--- app/src/main/java/com/example/mad_new/Input_marks.java
+++ app/src/main/java/com/example/mad_new/Input_marks.java
@@ -3,12 +3,83 @@ package com.example.mad_new;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.common.internal.safeparcel.SafeParcelable;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.snapshot.Index;
+
+import org.w3c.dom.Text;
 
 public class Input_marks extends AppCompatActivity {
+     private Button SUBMIT;
+     private EditText StudentFullName, Class, IndexNo, Subject1, Subject2, Subject3, Subject4,Subject5, TotalMarks, Average, Position;
+     DatabaseReference MADgroup;
+     BrighterBee brighterBee;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_marks);
+
+        SUBMIT = (Button) findViewById(R.id.btn3);
+        StudentFullName = (EditText) findViewById(R.id.editText1);
+        Class = (EditText) findViewById(R.id.editText2);
+        IndexNo = (EditText) findViewById(R.id.editText3);
+        Subject1 = (EditText) findViewById(R.id.editText4);
+        Subject2 = (EditText) findViewById(R.id.editText5);
+        Subject3 = (EditText) findViewById(R.id.editText6);
+        Subject4 = (EditText) findViewById(R.id.editText7);
+        Subject5 = (EditText) findViewById(R.id.editText8);
+        TotalMarks = (EditText) findViewById(R.id.editText9);
+        Average = (EditText) findViewById(R.id.editText10);
+        Position = (EditText) findViewById(R.id.editText11);
+        brighterBee = new BrighterBee();
+        MADgroup = FirebaseDatabase.getInstance().getReference().child("BrighterBee");
+
+        SUBMIT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int StudentName = Integer.parseInt(StudentFullName.getText().toString().trim());
+                int clas = Integer.parseInt(Class.getText().toString().trim());
+                int Index = Integer.parseInt(IndexNo.getText().toString().trim());
+                int Sub1Mark = Integer.parseInt(Subject1.getText().toString().trim());
+                int Sub2Mark = Integer.parseInt(Subject2.getText().toString().trim());
+                int Sub3Mark = Integer.parseInt(Subject3.getText().toString().trim());
+                int Sub4Mark = Integer.parseInt(Subject4.getText().toString().trim());
+                int Sub5Mark = Integer.parseInt(Subject5.getText().toString().trim());
+                int TotMarks  = Integer.parseInt(TotalMarks.getText().toString().trim());
+                int Avg = Integer.parseInt(Average.getText().toString().trim());
+                int Posi = Integer.parseInt(Position.getText().toString().trim());
+
+
+                brighterBee.setStudentName(StudentName);
+                brighterBee.setClas(clas);
+                brighterBee.setIndex(Index);
+                brighterBee.setSub1Mark(Sub1Mark);
+                brighterBee.setSub2Mark(Sub2Mark);
+                brighterBee.setSub3Mark(Sub3Mark);
+                brighterBee.setSub4Mark(Sub4Mark);
+                brighterBee.setSub5Mark(Sub5Mark);
+                brighterBee.setTotMarks(TotMarks);
+                brighterBee.setAvg(Avg);
+                brighterBee.setPosi(Posi);
+
+
+
+                MADgroup.push().setValue(brighterBee);
+                Toast.makeText(Input_marks.this,"Data insert successfully",Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
     }
 }
